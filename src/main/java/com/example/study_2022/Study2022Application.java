700da0fe08cd1162bd79c9c206af9fc01aeb71bb
jinx <jinXIN123>
2022-02-07 16:21:47 +0800

    first commit

---
+++ src/main/java/com/example/study_2022/Study2022Application.java
@@ -0,0 +1,13 @@
+package com.example.study_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study2022Application.class, args);
+    }
+
+}
